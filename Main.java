84bbca2a6b86818868a01f0ab0c2ec4f432e9477
Luis1505 <luis.montanez-vazquez@patelhs.org>
2020-12-02 13:57:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+   
+   String[] mrBarry = new String [12];
+   String[] msBenny = {"Alan","Ben","Dana","Ela","Gina","Hellen","Junior","Kathy","Luis","Mark","Oliva","Ruth"};
+  for(int index = 0; index < 10; index ++){
+    mrBarry[index] = msBenny[index];
+
+    System.out.println(mrBarry[index]);
+  } 
+  
+
+
+  }
+}
