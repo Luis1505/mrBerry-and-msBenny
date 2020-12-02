c05ff1d2a63c2a5c43db1c3a3f254c27f73ca3d0
Luis1505 <luis.montanez-vazquez@patelhs.org>
2020-12-02 13:59:54 +0000

    i finished

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
    
    String[] mrBarry = new String [12];
-   String[] msBenny = {"Alan","Ben","Dana","Ela","Gina","Hellen","Junior","Kathy","Luis","Mark","Oliva","Ruth"};
+   String[] msBenny = {"Alan","Ben","Dana","Ela","Gina","Hellen","Junior","Kathy","victor","Mark","Oliva","Ruth"};
   for(int index = 0; index < 10; index ++){
     mrBarry[index] = msBenny[index];
 
